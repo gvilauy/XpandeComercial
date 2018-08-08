a6a0d45a03a3b69515e3834f538fcb74361e14b3
Gabriel Vila <gabrielvila13@gmail.com>
2018-08-08 13:19:04 -0300

    OK

--- src/org/xpande/comercial/utils/ComercialUtils.java
+++ src/org/xpande/comercial/utils/ComercialUtils.java
@@ -1,6 +1,10 @@
 package org.xpande.comercial.utils;
 
 import org.adempiere.exceptions.AdempiereException;
+import org.compiere.model.I_C_Invoice;
+import org.compiere.model.MInvoice;
+import org.compiere.model.Query;
+import org.compiere.model.X_C_Invoice;
 import org.compiere.util.DB;
 
 import java.sql.PreparedStatement;
@@ -53,6 +57,28 @@ public final class ComercialUtils {
         }
 
         return value;
+    }
+
+
+    /***
+     * Metodo que obtiene y retorna una invoice según tipo de documento, número y socio de negocio recibidos.
+     * Xpande. Created by Gabriel Vila on 8/8/18.
+     * @param ctx
+     * @param cDocTypeID
+     * @param documentNo
+     * @param cBPartnerID
+     * @param trxName
+     * @return
+     */
+    public static MInvoice getInvoiceByDocPartner(Properties ctx, int cDocTypeID, String documentNo, int cBPartnerID, String trxName){
+
+        String whereClause = X_C_Invoice.COLUMNNAME_C_DocTypeTarget_ID + " =" + cDocTypeID +
+                " AND " + X_C_Invoice.COLUMNNAME_DocumentNo + " ='" + documentNo + "' " +
+                " AND " + X_C_Invoice.COLUMNNAME_C_BPartner_ID + " =" + cBPartnerID;
+
+        MInvoice model = new Query(ctx, I_C_Invoice.Table_Name, whereClause, trxName).first();
+
+        return model;
     }
 
 
